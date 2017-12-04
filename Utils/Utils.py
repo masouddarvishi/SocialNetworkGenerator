a0ea8bcf273f2ba8e02120aa227acf851f5b0c0e
Tiago Neto <ei12109@fe.up.pt>
2017-12-04 10:42:34 +0000

    Class is now holding all the stastitic data

--- Utils/Utils.py
+++ Utils/Utils.py
@@ -6,7 +6,18 @@ class StatsClass:
 
     def __init__(self, file_url):
         file = open(file_url, 'r')
-        self.data = json.load(file)
+        data = json.load(file)
+
+        self.races = []
+
+        for race in data["races"]:
+            new_race = RaceStat()
+            new_race.defineRace(race["race"], race["percentageOfTotal"])
+            new_race.definePercentage(race["description"]["SexualPreference"], race["description"]["Sex"],
+                                      race["description"]["SkinTone"], race["description"]["HairColor"])
+            self.races.append(new_race)
+
+
 
 class RaceStat:
     """ Class that holds the stats for a determined race"""
@@ -19,14 +30,14 @@ class RaceStat:
         self.percentageOfSkinTone = [] * 3
         self.percentageOfHairColor = [] * 5
 
-    def defineRace(self, race, percentageOfPop, percentageOfSex):
+    def defineRace(self, race, percentageOfPop):
         """ Defines race and percentage of population"""
         self.race = race
         self.percentageOfPopulation = percentageOfPop
-        self.percentageOfSex = percentageOfSex
 
-    def definePercentage(self, sexualPreference, skinTone, hairColor):
+    def definePercentage(self, sexualPreference, percentageOfSex, skinTone, hairColor):
         """ Defines the percentage of sexual preferences and physical appearance"""
         self.percentageOfSexualPreference = sexualPreference
+        self.percentageOfSex = percentageOfSex
         self.percentageOfSkinTone = skinTone
         self.percentageOfHairColor = hairColor
